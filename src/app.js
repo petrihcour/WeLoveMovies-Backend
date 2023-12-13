a8c997812453d4f38f92b1b874cd941ea624e5b9
Vanessa <zdperfectx@gmail.com>
2023-12-13 17:32:27 -0600

    two error handlers created. 404 and 500 in errors folder calling in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 if (process.env.USER) require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
+const notFound = require("./errors/notFound")
+const errorHandler = require("./errors/errorHandler");
+
+app.use(cors());
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
