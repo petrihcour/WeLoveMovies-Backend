29a4a1672920d81f66c7ba07331e66d7269b163b
Vanessa <zdperfectx@gmail.com>
2023-12-14 13:57:08 -0600

    cleaned up theaters.service. removed uneccessary promise in listtheatersandmovies function

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -4,9 +4,6 @@ function listTheatersAndMovies() {
     return knex("theaters as t")
     .leftJoin("movies_theaters as mt", "t.theater_id", "mt.theater_id")
     .leftJoin("movies as m", "mt.movie_id", "m.movie_id")
-    .then((theaters) => {
-        return theaters
-    })
 }
 
 function list() {
