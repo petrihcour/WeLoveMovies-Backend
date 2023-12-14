7b806058a5db357c0d143d6afbe9b5196bfe4191
Vanessa <zdperfectx@gmail.com>
2023-12-14 13:11:10 -0600

    added next back to error handler. that was causing issues. fixed

--- src/errors/errorHandler.js
+++ src/errors/errorHandler.js
@@ -1,4 +1,4 @@
-function errorHandler(error, req, res) {
+function errorHandler(error, req, res, next) {
     console.error(error);
     const { status = 500, message = "Something went wrong!" } = error;
     res.status(status).json({ error: message });
