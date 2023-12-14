b3b1539bee6f7468719b6af83d594125fb4308a8
Vanessa <zdperfectx@gmail.com>
2023-12-13 18:12:31 -0600

    completed list function for movies and tested

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,7 +2,8 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./movies.service");
 
 async function list(req, res) {
-
+    const movies = await service.list();
+    res.json({ data: movies });
 }
 
 module.exports = {
